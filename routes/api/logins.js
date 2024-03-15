4f09d029dd50bc05bc9e96d29ad9d19b57b32451
Corina Jewer <corina.jewer@keyin.com>
2024-03-15 15:04:53 -0230

    Completed Issue 15

--- routes/api/logins.js
+++ routes/api/logins.js
@@ -1,6 +1,6 @@
 var router = require('express').Router();
-//const loginsDal = require('../../services/pg.logins.dal')
-const loginsDal = require('../../services/m.logins.dal')
+const loginsDal = require('../../services/pg.logins.dal')
+//const loginsDal = require('../../services/m.logins.dal')
 
 // api/logins
 router.get('/', async (req, res) => {
@@ -16,5 +16,18 @@ router.get('/', async (req, res) => {
         res.json({message: "Service Unavailable", status: 503});
     }
 });
+
+router.get('/:id', async (req, res) => {
+    if(DEBUG) console.log('ROUTE: /api/logins/:id GET ' + req.url);
+    try {
+        let aLogin = await loginsDal.getLoginByLoginId(req.params.id); 
+        res.json(aLogin);
+    } catch {
+        // log this error to an error log file.
+        res.statusCode = 503;
+        res.json({message: "Service Unavailable", status: 503});
+    }
+});
+
 
 module.exports = router;
